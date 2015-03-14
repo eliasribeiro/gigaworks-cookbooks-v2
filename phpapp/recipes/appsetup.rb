6434a44dd370fa704461f5ce2d2649d7e9bfacc9
eliasribeiro <elias.ribeiro.s@gmail.com>
2015-03-14 14:51:25 -0300

    Update appsetup.rb

--- phpapp/recipes/appsetup.rb
+++ phpapp/recipes/appsetup.rb
@@ -7,38 +7,16 @@ node[:deploy].each do |app_name, deploy|
     code <<-EOH
     curl -sS https://getcomposer.org/installer | php
     php composer.phar install --no-dev
+    chmod -R 777 /srv/www
     EOH
   end
   
-  script "set_permissions" do
+  script "set_permissions_laravel" do
     interpreter "bash"
     user "root"
     code <<-EOH
     chmod -R 777 /srv/www/
     EOH
   end
-
-  template "#{deploy[:deploy_to]}/current/db-connect.php" do
-    source "db-connect.php.erb"
-    mode 0660
-    group deploy[:group]
-
-    if platform?("ubuntu")
-      owner "www-data"
-    elsif platform?("amazon")   
-      owner "apache"
-    end
-
-    variables(
-      :host =>     (deploy[:database][:host] rescue nil),
-      :user =>     (deploy[:database][:username] rescue nil),
-      :password => (deploy[:database][:password] rescue nil),
-      :db =>       (deploy[:database][:database] rescue nil),
-      :table =>    (node[:phpapp][:dbtable] rescue nil)
-    )
-
-   only_if do
-     File.directory?("#{deploy[:deploy_to]}/current")
-   end
-  end
+  
 end

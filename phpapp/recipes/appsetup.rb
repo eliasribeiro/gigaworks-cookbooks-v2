ea73cd114ffbf549fb2d6f85c80e03eb5cba3dc3
eliasribeiro <elias.ribeiro.s@gmail.com>
2015-03-14 16:02:35 -0300

    Update appsetup.rb

--- phpapp/recipes/appsetup.rb
+++ phpapp/recipes/appsetup.rb
@@ -2,10 +2,10 @@ node[:deploy].each do |app_name, deploy|
   script "install_composer" do
     interpreter "bash"
     user "root"
-    cwd "#{deploy[:deploy_to]}/current/vendor/laravel/framework/"
+    cwd "#{deploy[:deploy_to]}/current"
     code <<-EOH
-    curl -s https://getcomposer.org/installer | php
-    php composer.phar install
+    curl -sS https://getcomposer.org/installer | php
+    php composer.phar install --no-dev
     EOH
   end
 

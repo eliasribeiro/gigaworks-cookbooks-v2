cce7cfc605613b89531d1771b036830b6226a2c7
eliasribeiro <elias.ribeiro.s@gmail.com>
2015-03-14 15:29:35 -0300

    Update appsetup.rb

--- phpapp/recipes/appsetup.rb
+++ phpapp/recipes/appsetup.rb
@@ -17,6 +17,16 @@ node[:deploy].each do |app_name, deploy|
     chmod -R 777 storage
     EOH
   end
+  
+  if deploy[:application] == "platform"
+   script "set_permissions" do
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/current/app"
+    code <<-EOH
+    chmod -R 777 /srv/www
+    EOH
+  end
 
   template "#{deploy[:deploy_to]}/current/app/config/database.php" do
     source "database.php.erb"
